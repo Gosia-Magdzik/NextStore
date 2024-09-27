9005f9cedf33330d3ccf556104a0c52e8fa73740
Gosia Magdzik <melassa26@gmail.com>
2024-09-27 18:40:05 +0200

    button variant has been added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Button } from "@/components/ui/button"
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+          <h1 className='text-3xl'>Home Page</h1>
+          <Button 
+            variant='outline' 
+            size='lg' 
+            className='capitalize m-8'>
+              Click me
+          </Button>
+    </div>
   )
 }
 
