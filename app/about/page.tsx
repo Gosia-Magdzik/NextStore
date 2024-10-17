f926724ddeeaec979aa29f9c59e6414067384695
Gosia Magdzik <melassa26@gmail.com>
2024-10-17 20:00:21 +0200

    Prisma queries

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import db from '@/utils/db';
 
-function AboutPage() {
+async function AboutPage() {
+
+  const profile = await db.testProfile.create({
+    data:{
+      name:'prisma-client-js',
+    }
+  })
+
+  const users = await db.testProfile.findMany();
+
   return (
-    <div>AboutPage</div>
+    <div>
+      {users.map((user) => {
+        return <h2 key={user.id} className='text-2xl font-bold'>
+          {user.name}
+        </h2>
+      })}
+    </div>
   )
 }
 
