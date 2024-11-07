d06c75c6984de546d5f4a74517f55f4f9b7893a5
Gosia Magdzik <melassa26@gmail.com>
2024-11-07 18:56:53 +0100

    sectionTItle and emptyList Components

--- components/global/EmptyList.tsx
+++ components/global/EmptyList.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { cn } from '@/lib/utils'
 
-function EmptyList() {
+function EmptyList({
+  heading = 'No items found', 
+  className,
+}:{
+  heading?: string, 
+  className?:string
+}) {
   return (
-    <div>EmptyList</div>
+    <h2 className={cn('text-xl', className)}>
+      {heading}
+    </h2>
   )
 }
 
--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Separator } from '../ui/separator';
 
-function SectionTitle() {
+function SectionTitle({text}:{text:string}) {
   return (
-    <div>SectionTitle</div>
+    <div>
+      <h2 className='text-3xl font-medium tracking-wider capitalize mb-8'>
+        {text}
+      </h2>
+      <Separator/>
+    </div>
   )
 }
 
